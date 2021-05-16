abbbe9ddf42ee6f6b9a2e2b22254754b8eacb459
Allen_K <allenkelly278@gmail.com>
2021-05-16 21:33:58 +0100

    main container initial

--- src/containers/ComicViewer.jsx
+++ src/containers/ComicViewer.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ComicViewer = () => {
+
+    const [favourites, setFavourites] = useState(null);
+
+    useEffect( () => {
+        getTodaysComic();
+    }, []);
+
+    const getTodaysComic = () => {
+        fetch('http://xkcd.com/info.0.json')
+            .then(comic => setFavourites(comic.json()))
+    };
+
     return (
         <>
+            <h1>An XKCD tribute page</h1>
         </>
     );
 };
